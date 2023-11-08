3c1fe5f849108974ce666afb356f0e67f3821fd7
qtzz31712 <94wonhee@gmail.com>
2023-11-08 10:59:45 +0900

    resume my-batis작업 완료

--- src/main/java/team/pj/goodjob/resume/ResumeDao.java
+++ src/main/java/team/pj/goodjob/resume/ResumeDao.java
@@ -58,54 +58,14 @@ public class ResumeDao {
 	}
 
 	public ResumeVo selectResume(int r_no) {
-		String sql = "SELECT * FROM tbl_resume " + "WHERE r_no = ?";
 		List<ResumeVo> resumeVos = new ArrayList<ResumeVo>();
-
-		try {
-
-			resumeVos = jdbcTemplate.query(sql, new RowMapper<ResumeVo>() {
-
-				@Override
-				public ResumeVo mapRow(ResultSet rs, int rowNum) throws SQLException {
-
-					ResumeVo resumeVo = new ResumeVo();
-					resumeVo.setR_no(rs.getInt("r_no"));
-					resumeVo.setR_title(rs.getString("r_title"));
-					resumeVo.setR_edu(rs.getString("r_edu"));
-					resumeVo.setR_career(rs.getString("r_career"));
-					resumeVo.setR_career_date(rs.getInt("r_career_date"));
-					resumeVo.setR_mod_date(rs.getString("r_mod_date"));
-					resumeVo.setR_hope_loc(rs.getString("r_hope_loc"));
-					resumeVo.setR_hope_sct(rs.getString("r_hope_sct"));
-					resumeVo.setR_hope_hrd(rs.getString("r_hope_hrd"));
-					resumeVo.setR_hope_prd(rs.getString("r_hope_prd"));
-					resumeVo.setR_hope_day(rs.getString("r_hope_day"));
-					resumeVo.setR_hope_time(rs.getString("r_hope_time"));
-					resumeVo.setR_hope_pay(rs.getInt("r_hope_pay"));
-					resumeVo.setR_des(rs.getString("r_des"));
-					resumeVo.setR_u_no(rs.getInt("r_u_no"));
-					return resumeVo;
-				}
-
-			}, r_no);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-
-		}
-
+		resumeVos = sqlSession.selectList("mapper.resume.selectResume", r_no);
 		return resumeVos.size() > 0 ? resumeVos.get(0) : null;
 	}
 
 	public int deleteResume(int r_no) {
-		String sql = "DELETE FROM tbl_resume " + "WHERE r_no = ?";
 		int result = -1;
-		try {
-			result = jdbcTemplate.update(sql, r_no);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println(result);
+	result = sqlSession.delete("mapper.resume.deleteResume", r_no);
 		return result;
 	}
 
